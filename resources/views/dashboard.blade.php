dcb83c4def12a39876399ce240a86fa89bcf990f
Muhammad Zohaib Shabbir <zuhaibranaa@gmail.com>
2021-04-09 17:56:32 +0500

    databases improved relational model

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,65 +4,80 @@
 
 <div class="row">
 
-@include('sidebar')
-
-
+  @include('sidebar')
   <div class="col-sm-3">
-    <div class="card">
+    <div class="card bg-success text-light m-2">
+
+
       <div class="card-body">
-        <h5 class="card-title">Posts</h5>
-        <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-stickies" viewBox="0 0 16 16">
-          <path d="M1.5 0A1.5 1.5 0 0 0 0 1.5V13a1 1 0 0 0 1 1V1.5a.5.5 0 0 1 .5-.5H14a1 1 0 0 0-1-1H1.5z"/>
-          <path d="M3.5 2A1.5 1.5 0 0 0 2 3.5v11A1.5 1.5 0 0 0 3.5 16h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 16 9.586V3.5A1.5 1.5 0 0 0 14.5 2h-11zM3 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V9h-4.5A1.5 1.5 0 0 0 9 10.5V15H3.5a.5.5 0 0 1-.5-.5v-11zm7 11.293V10.5a.5.5 0 0 1 .5-.5h4.293L10 14.793z"/>
-        </svg>
+        <div class="row justify-content-between">
+          <i class="fa fa-file-text fa-5x" aria-hidden="true"></i>
+          <div class="text-right">
+            <div class="huge">0</div>
+            <div>Posts</div>
+          </div>
+        </div>
       </div>
-      <hr/>
-      <div onclick="window.location = '/posts';" class="btn btn-primary">
-        <a class="btn">Details
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-          </svg>
+
+      <div class="card-footer">
+        <a href="/posts" class="d-flex justify-content-between text-dark">
+          <span>Details</span>
+          <span><i class="fa fa-arrow-circle-right"></i></span>
         </a>
       </div>
     </div>
+
   </div>
 
   <div class="col-sm-3">
-    <div class="card">
+    <div class="card bg-primary text-light m-2">
+
+
       <div class="card-body">
-        <h5 class="card-title">Comments</h5>
-        <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-chat-right" viewBox="0 0 16 16">
-          <path d="M2 1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h9.586a2 2 0 0 1 1.414.586l2 2V2a1 1 0 0 0-1-1H2zm12-1a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z"/>
-        </svg>
+        <div class="row justify-content-between">
+          <i class="fa fa-comments fa-5x" aria-hidden="true"></i>
+          <div class="text-right">
+            <div class="huge">
+
+            </div>
+            <div>Comments</div>
+          </div>
+        </div>
       </div>
-      <hr/>
-      <div onclick="window.location = '/comment';" class="btn btn-primary">
-        <a class="btn">Details
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-          </svg>
+
+      <div class="card-footer">
+        <a href="/comment" class="d-flex justify-content-between text-dark">
+          <span>Details</span>
+          <span><i class="fa fa-arrow-circle-right"></i></span>
         </a>
       </div>
     </div>
+
   </div>
 
   <div class="col-sm-3">
-    <div class="card">
+    <div class="card bg-warning text-light m-2">
+
+
       <div class="card-body">
-        <h5 class="card-title">Profile</h5>
-        <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
-          <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"/>
-        </svg>
+        <div class="row justify-content-between">
+          <i class="fa fa-users fa-5x" aria-hidden="true"></i>
+          <div class="text-right text-dark">
+            <div class="huge">{{count(App\Models\User::all())}} Users</div>
+            <div>Edit Profile</div>
+          </div>
+        </div>
       </div>
-<hr/>
-      <div onclick="window.location = '/profile';" class="btn btn-primary">
-        <a class="btn">Details
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-          </svg>
+
+      <div class="card-footer">
+        <a href="/profile" class="d-flex justify-content-between text-dark">
+          <span>Edit Your Profile</span>
+          <span><i class="fa fa-arrow-circle-right"></i></span>
         </a>
       </div>
     </div>
+
   </div>
+</div>
 </div>
 @endsection
